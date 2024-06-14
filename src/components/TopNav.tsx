f8a6546616624ea816e0137fbff8ffa932cda33b
thejayduck <ardafevzi.armutcu@gmail.com>
2024-06-14 23:43:08 +0200

    Adds top nav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,31 +1,53 @@
-import React, { PureComponent } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/TopNav.module.scss";
-import "remixicon/fonts/remixicon.css";
 import Button from "./Button";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { debounce } from "@/app/helper";
 
-const TopNav: React.FC = () => {
+interface TopNavProps {
+  containerRef: React.MutableRefObject<HTMLDivElement | null>;
+}
+
+const TopNav: React.FC<TopNavProps> = ({ containerRef }) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      if (containerRef.current) {
+        const scrollPosition = containerRef.current.scrollTop > 100;
+        setShow(scrollPosition);
+      }
+    }, 150);
+
+    window.addEventListener("scroll", handleScroll, { capture: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
   return (
-    <motion.div
-      className={styles.topNav}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <div className={styles.title}>
-        <span>
-          <span className={styles.highlight}>Fluid</span>Noti
-        </span>
-      </div>
-      <div className={styles.buttons}>
-        <Button
-          text="Buy Now"
-          icon="ri-shopping-cart-2-line"
-          href="https://crissnb.gumroad.com/l/fluidnoti"
-          purchaseButton={true}
-        />
-      </div>
-    </motion.div>
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className={styles.topNav}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className={styles.title}>
+            <span>
+              <span className={styles.highlight}>Fluid</span>Noti
+            </span>
+          </div>
+          <div className={styles.buttons}>
+            <Button
+              text="Buy Now"
+              icon="ri-shopping-cart-2-line"
+              href="https://crissnb.gumroad.com/l/fluidnoti"
+              purchaseButton={true}
+            />
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
